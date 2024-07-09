95d17c6f4cff1e7818cacc133e436cba61be30f2
$.B <159635318+oldmxnk@users.noreply.github.com>
2024-07-09 11:12:34 +0100

    Minor tweaks and updates.

--- GenerateInventory.ps1
+++ GenerateInventory.ps1
@@ -1,13 +1,20 @@
 #
 # Generate Ansible Inventory file for Hyper-V VMs
 #
-$vmInfo  = Get-VM | Select-Object -ExpandProperty NetworkAdapters
+
+$InvFile = "ansible_hyperv.list"
+
+$PrjDirPrefix = "Project"
+$vmNamePrefix = $PrjDirPrefix + "*"
+
+$vmInfo = Get-VM -Name $vmNamePrefix | Select-Object -ExpandProperty NetworkAdapters
+
 Write-Host
 Write-Host "Generating Inventory file..."
 Write-Host
 
 Write-Host "[hyperv]"
-Set-Content -Path "ansible_hyperv.list" -Value "[hyperv]"
+Set-Content -Path $InvFile -Value "[hyperv]"
 
 $vmInfo | ForEach-Object {
 	$vmName = $_.VMName
@@ -15,29 +22,30 @@ $vmInfo | ForEach-Object {
 
 	if ([string]::IsNullOrEmpty($ipAddress)) {
 		Write-Host $vmName
-		Add-Content -Path "ansible_hyperv.list" -Value $vmName
+		Add-Content -Path $InvFile -Value $vmName
 	} else {
 		Write-Host (($vmName, " ", "ansible_host=", $ipAddress) -join "")
-		Add-Content -Path "ansible_hyperv.list" -Value "$vmName ansible_host=$ipAddress"
+		Add-Content -Path $InvFile -Value "$vmName ansible_host=$ipAddress"
 	}
 }
 
-Add-Content -Path "ansible_hyperv.list" -Value ""
-Add-Content -Path "ansible_hyperv.list" -Value "[all:vars]"
-Add-Content -Path "ansible_hyperv.list" -Value "ansible_connection=ssh"
-Add-Content -Path "ansible_hyperv.list" -Value "ansible_user=vagrant"
-Add-Content -Path "ansible_hyperv.list" -Value "ansible_ssh_pass=London01"
-Add-Content -Path "ansible_hyperv.list" -Value "ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
+Add-Content -Path $InvFile -Value ""
+Add-Content -Path $InvFile -Value "[all:vars]"
+Add-Content -Path $InvFile -Value "ansible_connection=ssh"
+Add-Content -Path $InvFile -Value "ansible_user=vagrant"
+Add-Content -Path $InvFile -Value "ansible_ssh_pass=London01"
+Add-Content -Path $InvFile -Value "ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
 
 #
 # Copy File to Ansible Control Server if Running.
 #
-$vmName  = "Debian"
-$vmState = Get-VM -Name $vmName | Select-Object -ExpandProperty State
+
+$AnsibleVM = "Debian"
+$vmState = Get-VM -Name $AnsibleVM | Select-Object -ExpandProperty State
 
 if ($vmState -eq "Running") {
-	Copy-VMFile -Name "Debian" -SourcePath "ansible_hyperv.list" -DestinationPath "/home/patrol" -FileSource Host -Force
+	Copy-VMFile -Name "Debian" -SourcePath $InvFile -DestinationPath "/home/patrol" -FileSource Host -Force
 	Write-Host
-	Write-Host "Inventory file copied to /home/patrol on Host $vmName."
+	Write-Host "Inventory file $InvFile copied to /home/patrol on Host $AnsibleVM."
 	Write-Host
 }
